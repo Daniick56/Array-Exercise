e3bfb173b8632c71cc1fd4e32bddac665e1f5d10
Daniick56 <danielballesteros22@outlook.com>
2024-03-05 17:58:48 -0500

    creando un array con variedad de datos

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ let emptyArray = [];
 // creando un array con elementos
 let frutas = ["banano", "manzana", "papaya"];
 
+// creando un array con variedad de datos
+let variety = ["Daniel", 20, "alto", 10];
+
