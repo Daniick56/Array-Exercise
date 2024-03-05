58deebccebb0787fd4527abb8e255afbd1a60601
Daniick56 <danielballesteros22@outlook.com>
2024-03-05 18:02:13 -0500

    creando un array con el constructor

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ let frutas = ["banano", "manzana", "papaya"];
 // creando un array con variedad de datos
 let variety = ["Daniel", 20, "alto", 10];
 
+//creando un array con el constructor Array();
+let colors = Array("red", "green", "purple");
+
